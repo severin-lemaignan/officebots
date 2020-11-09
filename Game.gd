e8e2137dee8700905cc2191a002db18150a53cc4
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2020-11-09 13:37:40 +0000

    Define spawn points for the players and limit max # of players
    The order of the spawn points is randomised on the server, so that we always start somewhere different

--- Game.gd
+++ Game.gd
@@ -10,6 +10,9 @@ var is_server : bool
 
 var local_player
 
+# if changing that, make sure to add spawn points accordingly
+var MAX_PLAYERS = 10
+
 export(String) var username = "John Doe"
 
 # Player info, associate ID to data
@@ -20,6 +23,8 @@ var my_info =  { "name": username, "favorite_color": Color8(255, 0, 255) }
 
 
 func _ready():
+    
+    randomize()
     
     var arguments = {"server": false}
     for argument in OS.get_cmdline_args():
@@ -48,6 +53,7 @@ func _ready():
         # the last 'true' parameter enables the Godot high-level multiplayer API
         peer.listen(SERVER_PORT, PoolStringArray(), true)
         
+        shuffle_spawn_points()
         local_player = $FakePlayer
         
     else:
@@ -80,6 +86,13 @@ func _process(_delta):
     get_tree().network_peer.poll()
 
 
+func shuffle_spawn_points():
+    var order = [0,1,2,3,4,5,6,7,8,9].shuffle()
+    
+    for p in range(10):
+        var child = $SpawnPointsPlayers.get_child(order[p])
+        $SpawnPointsPlayers.move_child(child, p)
+        
 
 ##### NETWORK SIGNALS HANDLERS #####
 # only triggered client-side
@@ -150,9 +163,17 @@ remote func pre_configure_game():
     
     print("Done pre-configuring game. Waiting for the server to un-pause me...")
 
-remote func post_configure_game():
-    print("Starting the game!")
-    get_tree().set_pause(false)
+# server has accepted our player, we can start the game.
+# *if transform=null, the server has rejected our player*
+# (typically because no more space). In which case, we must exit.
+remote func post_configure_game(transform):
+    if !transform:
+        print("Server is full! Exiting")
+        get_tree().quit()
+    else:
+        print("Starting the game!")
+        local_player.transform = transform
+        get_tree().set_pause(false)
 
 func remove_player(id):
     player_info[id]["object"].queue_free()
@@ -179,6 +200,11 @@ remote func done_preconfiguring(who):
     assert(who in player_info) # Exists
     assert(not who in players_done) # Was not added yet
 
+    if players_done.size() == MAX_PLAYERS:
+        print("Game full! Can not accept any extra player!")
+        rpc_id(who, "post_configure_game", null)
+        return
+        
     print(player_info[who]["name"] + " is ready.")
     players_done.append(who)
     
@@ -187,6 +213,8 @@ remote func done_preconfiguring(who):
     #    print("All players ready, starting the game!")
     #    rpc_id(who, "post_configure_game")
     
-    # start the game immediately for whoever is connecting
-    rpc_id(who, "post_configure_game")
+    # start the game immediately for whoever is connecting, passing the
+    # start location of the player
+    var start_location = $SpawnPointsPlayers.get_child(players_done.size() - 1).transform
+    rpc_id(who, "post_configure_game", start_location)
 
