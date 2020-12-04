064e066e5a38577b5828b1f18d338c6ec8cc0c05
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2020-12-04 11:01:41 +0000

    Struggled to get a correct display of the laser scan, found a workaround
    See comment in code for details

--- LaserScannerLines.gd
+++ LaserScannerLines.gd
@@ -15,9 +15,10 @@ func _ready():
 
         var ray = ray_class.instance()
         ray.name = "ray_" + str(i)
-        ray.transform.origin = Vector3(0,0,0)
-        ray.set_angle(angle)
         add_child(ray)
+        
+        ray.transform.origin = Vector3(0,0,0)
+        ray.rotate_y(angle)
         
         angle += inc
 
@@ -26,28 +27,38 @@ func draw(ranges):
     if visible:
         for i in range(NB_RAYS):
             var ray = get_child(i)
-            
-            if ranges[i] < 0:
+            #if ranges[i] < 0:
+            if ranges[i] == Vector3(0,0,0):
                 ray.visible = false
             else:
                 ray.visible = true
-                ray.set_distance(ranges[i])
+                #ray.set_distance(ranges[i])
+                ray.set_hitpoint(ranges[i])
 
 
 func laser_scan():
+    #get_parent().game_instance.debug_point(global_transform.origin)
     var space_state = get_world().direct_space_state
     var laser_ranges = []
+    var laser_hitpoints = []
     
-    var theta = PI / (NB_RAYS + 1)
+    var inc = PI / (NB_RAYS - 1)
+    var angle = -PI/2
+
     for i in range(NB_RAYS):
-        var angle = theta * (i + 1)
         var target = global_transform.basis.xform(global_transform.origin + Vector3(0,0,RANGE).rotated(Vector3(0,1,0), angle))
         var result = space_state.intersect_ray(global_transform.origin, target)
         if result:
+            #get_parent().game_instance.debug_point(result.position)
             laser_ranges.append(global_transform.origin.distance_to(result.position))
+            laser_hitpoints.append(result.position - global_transform.origin)
         else:
             laser_ranges.append(-1)
+            laser_hitpoints.append(Vector3(0,0,0))
+            
+        angle += inc
 
-    draw(laser_ranges)
+    #draw(laser_ranges)
+    draw(laser_hitpoints)
     
     return laser_ranges
--- Ray.gd
+++ Ray.gd
@@ -1,11 +1,20 @@
 extends Spatial
 
-
-
-func set_angle(angle):
-    rotation = Vector3(0,angle,0)
-
+# this does not display the hit points at the right position, for an unclear reason.
+# it looks as if the coordinates resulting from the rotation of Ray (in LaserScannerLines.gd) followed by
+# the translation are transformed again somehow, following a weird inverse function... spent an hour
+# trying to figure it out, to no avail
 func set_distance(dist):
-    $Impact.translation = Vector3(dist,0,0)
-    $RaySurface.scale = Vector3(dist,0,0.02)
-    $RaySurface.translation = Vector3(dist/2,0,0)
+    $Impact.translation = Vector3(0,0,dist)
+    $RaySurface.scale = Vector3(0.02,0,dist)
+    $RaySurface.translation = Vector3(0,0,dist/2)
+
+# z points forward
+func set_hitpoint(pos : Vector3):
+    $Impact.global_transform.origin = global_transform.origin + pos
+    #var dist = pos.length()
+    #$Impact.translation = Vector3(0, 0, dist)
+    
+    #$RaySurface.scale = Vector3(0.02,0,dist)
+    #$RaySurface.translation = Vector3(0,0,dist/2)
+    
