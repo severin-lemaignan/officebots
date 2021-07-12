29437c023bf33bb7b9a2ced9b90cb2846a04a6f3
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2021-07-12 10:58:48 +0100

    avoid duplication of 'is_speaking' code

--- Character.gd
+++ Character.gd
@@ -8,7 +8,6 @@ onready var anim_player = $AnimationPlayer
 # are oriented to face this player
 var local_player
 var expression = 0 #to export the expression 
-var is_speaking=false 
 onready var speech_bubble = $SpeechBubbleHandle/SpeechBubble
 onready var speech_bubble_handle = $SpeechBubbleHandle
 onready var face = $Face # used by Robot.gd to compute visibility of players
@@ -173,10 +172,11 @@ remote func execute_move_and_slide(linear_velocity):
 remote func execute_puppet_says(msg):
     assert(get_tree().is_network_server())
     
-    is_speaking=true
-    $Timer_is_speaking.start()
-    
     rpc("puppet_says", msg)
+    
+    # execute it as well on the server, for debugging purpose + to track when the players are
+    # speaking in the logs
+    say(msg)
     
 
 remote func execute_puppet_set_expression(msg):
@@ -187,7 +187,7 @@ remote func execute_puppet_set_expression(msg):
 
 # physics process is only enabled on the server
 func _physics_process(delta):
-
+    
     velocity.y += GameState.GRAVITY * delta
   
     velocity = move_and_slide(velocity, Vector3.UP)
@@ -320,7 +320,7 @@ func on_rotation_finished():
     target_quaternion = null
     quaternion_slerp_progress = 0
     
-func say(text, wait_time=2):
+func say(text, wait_time=4):
     speech_bubble.say(text, speech_bubble.ButtonType.NONE, wait_time)
 
 func distance_to(object):
@@ -370,3 +370,4 @@ func quaternions_distance(q1, q2):
 # Once the client is speaking, the var is_speaking is true for the next 5 seconds 
 func _on_Timer_is_speaking_timeout():
     is_speaking=false
+
