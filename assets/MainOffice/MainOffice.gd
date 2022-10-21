82e1980ddf46d025545fbe11a8da8c0d63788dbb
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-10-21 11:05:46 +0900

    minor tweaks mainoffice scene:

--- assets/MainOffice/MainOffice.gd
+++ assets/MainOffice/MainOffice.gd
@@ -5,30 +5,30 @@ onready var nav = $Navigation
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+	pass # Replace with function body.
 
 # called by Game.gd upon player creation
 func set_local_player(object):
-    
-    for d in $Doors.get_children():
-        d.local_player = object
-    
-    for d in $PickableObjects.get_children():
-        d.local_player = object
+	
+	for d in $Doors.get_children():
+		d.local_player = object
+	
+	for d in $PickableObjects.get_children():
+		d.local_player = object
 
 
 func get_navmesh():
-    var navmesh = $Navigation/NavigationMeshInstance.navmesh
-    var polygons = []
-    var vertices = navmesh.get_vertices()
-    
-    for idx in navmesh.get_polygon_count():
-        var polygon = []
-        for v in navmesh.get_polygon(idx):
-            var vertex = GameState.convert_coordinates_godot2robotics(global_transform.xform(vertices[v]))
-            polygon.append([vertex.x, vertex.y, vertex.z])
-        
-        polygons.append(polygon)
-    
-    return polygons
+	var navmesh = $Navigation/NavigationMeshInstance.navmesh
+	var polygons = []
+	var vertices = navmesh.get_vertices()
+	
+	for idx in navmesh.get_polygon_count():
+		var polygon = []
+		for v in navmesh.get_polygon(idx):
+			var vertex = GameState.convert_coordinates_godot2robotics(global_transform.xform(vertices[v]))
+			polygon.append([vertex.x, vertex.y, vertex.z])
+		
+		polygons.append(polygon)
+	
+	return polygons
 
